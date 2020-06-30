773d6cd20b5ab41ff723b32de864a79c08f9294c
kamui-7 <65237048+kamui-7@users.noreply.github.com>
2020-06-30 15:03:13 -0500

    updated gifs

--- app/src/main/java/com/example/dictionaryapp/MainActivity.java
+++ app/src/main/java/com/example/dictionaryapp/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
