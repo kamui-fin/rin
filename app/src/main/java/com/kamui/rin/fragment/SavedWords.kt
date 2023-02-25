2e669b30125853ccc83ca0ee38bfcd1c12b6e220
Kamui <fin-kamui@pm.me>
2023-02-25 16:52:37 -0600

    refactor: saved words logic into VM

--- app/src/main/java/com/kamui/rin/fragment/SavedWords.kt
+++ app/src/main/java/com/kamui/rin/fragment/SavedWords.kt
@@ -2,34 +2,62 @@ package com.kamui.rin.fragment
 
 import android.app.AlertDialog
 import android.content.*
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler.Callback
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.preference.Preference
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.*
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.kamui.rin.DictEntryAdapter
-import com.kamui.rin.R
 import com.kamui.rin.databinding.FragmentSavedWordsBinding
-import com.kamui.rin.databinding.LayoutListentryBinding
 import com.kamui.rin.databinding.SavedWordsItemBinding
+import com.kamui.rin.db.DBHelper
+import com.kamui.rin.db.DictEntry
 import com.kamui.rin.util.Settings
-import kotlinx.coroutines.NonDisposableHandle.parent
-import java.nio.charset.Charset
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-val items = mutableListOf("その", "後", "いつ", "定期", "購入", "商品", "未開封", "連絡", "電話", "メール")
+data class SavedWordsState(
+    val words: List<String> = listOf("その", "後", "いつ", "定期", "購入", "商品", "未開封", "連絡", "電話", "メール")
+)
+
+class SavedWordsViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(SavedWordsState())
+    val uiState: StateFlow<SavedWordsState> = _uiState.asStateFlow()
+
+    fun deleteItem(index: Int) {
+        _uiState.update { state ->
+            state.copy(words = state.words.subList(0,index) + state.words.subList(index + 1, state.words.size))
+        }
+    }
+
+    fun saveWordsToFile(fileUri: Uri, resolver: ContentResolver?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val wordListString = uiState.value.words.joinToString("\n")
+            resolver?.takePersistableUriPermission(fileUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            val outputStream = resolver?.openOutputStream(fileUri, "wt")
+            outputStream?.write(wordListString.toByteArray())
+            outputStream?.close()
+        }
+    }
+}
+
 
 class SavedWords : Fragment() {
     private var _binding: FragmentSavedWordsBinding? = null
     private val binding get() = _binding!!
+
+     val wordsViewModel: SavedWordsViewModel by viewModels()
 
     private lateinit var settings: Settings
 
@@ -43,71 +71,75 @@ class SavedWords : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSavedWordsBinding.inflate(inflater, container, false)
+
+        binding.wordListRecycler.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        binding.wordListRecycler.layoutManager = LinearLayoutManager(context)
+        val itemTouchHelper = ItemTouchHelper(ItemTouchCallback())
+        itemTouchHelper.attachToRecyclerView(binding.wordListRecycler)
+
+        binding.saveButton.setOnClickListener { saveToFile() }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                wordsViewModel.uiState.collect {
+                    binding.wordListRecycler.adapter = Adapter()
+                }
+            }
+        }
+
+
         return binding.root
+    }
+
+    private fun saveToFile() {
+        val fileUri = settings.savedWordsPath
+        if (fileUri != null) {
+            wordsViewModel.saveWordsToFile(fileUri, activity?.contentResolver)
+        } else {
+            val alertDialog = AlertDialog.Builder(activity)
+                .setMessage("Specify a file in settings")
+                .setPositiveButton("OK") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+                .create()
+            alertDialog.show()
+        }
+    }
+
+    inner class ItemTouchCallback: ItemTouchHelper.Callback() {
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            return makeMovementFlags(0, ItemTouchHelper.RIGHT)
+        }
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.absoluteAdapterPosition
+            wordsViewModel.deleteItem(pos)
+        }
     }
 
     inner class ViewHolder(val binding: SavedWordsItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.wordListRecycler.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        binding.wordListRecycler.layoutManager = LinearLayoutManager(context)
-        binding.wordListRecycler.adapter = object : RecyclerView.Adapter<SavedWords.ViewHolder>() {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedWords.ViewHolder {
-                val binding = SavedWordsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                return ViewHolder(binding)
-            }
-
-            override fun getItemCount(): Int {
-                return items.size
-            }
-
-            override fun onBindViewHolder(holder: SavedWords.ViewHolder, position: Int) {
-                holder.binding.word.text = items[position]
-//                val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//                val clip: ClipData = ClipData.newPlainText("Word", items[position])
-//                clipboard.setPrimaryClip(clip)
-//                Toast.makeText(context, "Copied ${items[position]} to clipboard", Toast.LENGTH_SHORT).show()
-            }
+    inner class Adapter : RecyclerView.Adapter<SavedWords.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedWords.ViewHolder {
+            val binding = SavedWordsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ViewHolder(binding)
         }
-        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
-            override fun getMovementFlags(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder
-            ): Int {
-                return makeMovementFlags(0, ItemTouchHelper.RIGHT)
-            }
 
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
+        override fun getItemCount(): Int {
+            return wordsViewModel.uiState.value.words.size
+        }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val pos = viewHolder.absoluteAdapterPosition
-                items.removeAt(pos)
-                (binding.wordListRecycler.adapter as RecyclerView.Adapter).notifyItemRemoved(pos)
-            }
-        })
-        itemTouchHelper.attachToRecyclerView(binding.wordListRecycler)
-
-        binding.saveButton.setOnClickListener {
-            val fileUri = settings.savedWordsPath
-            if (fileUri != null) {
-                val wordListString = items.joinToString("\n")
-                activity?.contentResolver?.takePersistableUriPermission(fileUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                val outputStream = activity?.contentResolver?.openOutputStream(fileUri, "wt")
-                outputStream?.write(wordListString.toByteArray())
-                outputStream?.close()
-            } else {
-                val alertDialog = AlertDialog.Builder(activity)
-                    .setMessage("Specify a file in settings")
-                    .setPositiveButton("OK") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
-                    .create()
-                alertDialog.show()
-            }
+        override fun onBindViewHolder(holder: SavedWords.ViewHolder, position: Int) {
+            holder.binding.word.text = wordsViewModel.uiState.value.words[position]
         }
     }
 }
